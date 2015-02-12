23b9cd7aed13edd2b1e6456df57e0a8c44d84114
Ruedi Steinmann <ruediste@gmail.com>
2015-02-12 19:07:29 +0100

    make property map freezable

--- src/main/java/com/github/ruediste/attachedProperties4J/AttachedPropertyMap.java
+++ src/main/java/com/github/ruediste/attachedProperties4J/AttachedPropertyMap.java
@@ -15,53 +15,82 @@ import java.util.HashMap;
 public class AttachedPropertyMap {
 
 	private HashMap<AttachedProperty<?, ?>, Object> map = new HashMap<AttachedProperty<?, ?>, Object>();
+	boolean frozen = false;
 
 	@SuppressWarnings("unchecked")
 	synchronized <T> T get(AttachedProperty<?, T> key) {
 		return (T) map.get(key);
 	}
 
+	private void checkNotFrozen() {
+		if (frozen)
+			throw new IllegalStateException(
+					"Attempt to modify frozen AttachedPropertyBearer");
+	}
+
 	synchronized <T> void set(AttachedProperty<?, T> key, T value) {
+		checkNotFrozen();
 		map.put(key, value);
 	}
 
 	synchronized void clear(AttachedProperty<?, ?> key) {
+		checkNotFrozen();
 		map.remove(key);
 	}
 
 	synchronized boolean isSet(AttachedProperty<?, ?> key) {
 		return map.containsKey(key);
 	}
+
 	public synchronized void clearAll() {
+		checkNotFrozen();
 		map.clear();
 	}
 
 	@Override
-	public int hashCode() {
+	public synchronized int hashCode() {
 		return map.hashCode();
 	}
-	
-	public void putAll(AttachedPropertyBearer other){
+
+	public synchronized void putAll(AttachedPropertyBearer other) {
+		checkNotFrozen();
 		putAll(other.getAttachedPropertyMap());
 	}
-	
-	public void putAll(AttachedPropertyMap other){
+
+	public synchronized void putAll(AttachedPropertyMap other) {
+		checkNotFrozen();
 		map.putAll(other.map);
 	}
+
+	public synchronized boolean isFrozen() {
+		return frozen;
+	}
+
+	public synchronized void freeze() {
+		frozen = true;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (obj==this)
+		if (obj == this)
 			return true;
-		if (obj==null)
+		if (obj == null)
 			return false;
-		if (!getClass().equals(obj.getClass())){
+		if (!getClass().equals(obj.getClass())) {
 			return false;
 		}
-		
-		AttachedPropertyMap other=(AttachedPropertyMap) obj;
-		return map.equals(other.map);
+
+		AttachedPropertyMap other = (AttachedPropertyMap) obj;
+
+		HashMap<AttachedProperty<?, ?>, Object> otherMap;
+		synchronized (other) {
+			otherMap = new HashMap<>(other.map);
+		}
+		synchronized (this) {
+			return map.equals(otherMap);
+		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return map.toString();
